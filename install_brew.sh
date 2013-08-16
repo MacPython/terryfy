a08fdea829f1dea8478182fa5fe7b86cedddce43
Matt Terry <matt.terry@gmail.com>
2013-08-16 15:14:38 -0700

    close to building matplotlib

--- install_brew.sh
+++ install_brew.sh
@@ -7,8 +7,7 @@ then
     brew install python
 
     pip install nose numpy
-#    brew install freetype libpng pkg-config
-#    pip install matplotlib
+    brew install freetype libpng pkg-config
 elif [ "$TEST" == "brew_system" ]
 then
     brew update
@@ -16,8 +15,7 @@ then
     # use system python
 
     pip install nose numpy
-#    brew install freetype libpng pkg-config
-#    pip install matplotlib
+    brew install freetype libpng pkg-config
 elif [ "$TEST" == "brew_py3" ]
 then
     brew update
@@ -25,8 +23,33 @@ then
     brew install python3
 
     pip3 install nose numpy
-#    brew install freetype libpng pkg-config
-#    pip install matplotlib
+    brew install freetype libpng pkg-config
+elif [ "$TEST" == "macports_py26" ]
+then
+    MACPORTS="MacPorts-2.2.0"
+    PREFIX=/opt/local
+    M_dot_m="2.6"
+
+    Mm=`echo $M_dot_m | tr -d '.'`
+    PY="py$Mm"
+
+    curl https://distfiles.macports.org/MacPorts/$MACPORTS.tar.gz > $MACPORTS.tar.gz --insecure
+    tar -xzf $MACPORTS.tar.gz
+
+    cd $MACPORTS
+    ./configure --prefix=$PREFIX
+    make 
+    sudo make install
+    cd ..
+
+    export PATH=$PREFIX/bin:$PATH
+    sudo port -v selfupdate
+
+    sudo port install python$Mm $PY-nose $PY-pip $PY-numpy libpng freetype
+    sudo port select python python$M_dot_m
+    sudo port select nosetests nosetests-$M_dot_m
 else
     echo "Unknown test setting ($TEST)"
 fi
+
+pip install matplotlib
