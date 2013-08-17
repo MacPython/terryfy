d18880c36f757eb207ccc715114151e37300de7f
Matt Terry <matt.terry@gmail.com>
2013-08-16 19:54:16 -0700

    macports functions, more macports stubs

--- install.sh
+++ install.sh
@@ -1,6 +1,51 @@
 #!/usr/bin/env sh
 
-if [ "$TEST" == "brew_py" ]
+function install_macports {
+    PREFIX=$1
+    MACPORTS="MacPorts-2.2.0"
+    curl https://distfiles.macports.org/MacPorts/$MACPORTS.tar.gz > $MACPORTS.tar.gz --insecure
+    tar -xzf $MACPORTS.tar.gz
+
+    cd $MACPORTS
+    ./configure --prefix=$PREFIX
+    make 
+    sudo make install
+    cd ..
+
+    export PATH=$PREFIX/bin:$PATH
+    sudo port -v selfupdate
+}
+
+
+function port_install_python {
+    #major.minor version
+    M_dot_m=$1
+    Mm=`echo $M_dot_m | tr -d '.'`
+    PY="py$Mm"
+
+    sudo port install python$Mm $PY-numpy libpng freetype
+    sudo port select --set python python$Mm
+
+    # remove for mpl
+    sudo port install $PY-nose $PY-pip  # remove for mpl
+    sudo ln -s $PREFIX/bin/nosetests-$M_dot_m $PREFIX/bin/nosetests
+    sudo ln -s $PREFIX/bin/pip-$M_dot_m $PREFIX/bin/pip
+}
+
+
+if [ "$TEST" == "brew_system" ]
+then
+    brew update
+
+    # use system python, numpy
+
+    sudo easy_install pip
+    brew install freetype libpng pkg-config
+
+    # remove for mpl       
+    sudo pip install nose matplotlib
+
+elif [ "$TEST" == "brew_py" ]
 then
     brew update
 
@@ -12,17 +57,7 @@ then
     # remove for mpl
     pip install nose  
     pip install matplotlib
-elif [ "$TEST" == "brew_system" ]
-then
-    brew update
 
-    # use system python, numpy
-
-    sudo easy_install pip
-    brew install freetype libpng pkg-config
-
-    # remove for mpl       
-    sudo pip install nose matplotlib
 elif [ "$TEST" == "brew_py3" ]
 then
     brew update
@@ -35,37 +70,42 @@ then
     # remove for mpl
     pip3 install nose  
     pip3 install matplotlib
+
 elif [ "$TEST" == "macports_py26" ]
 then
-    MACPORTS="MacPorts-2.2.0"
     PREFIX=/opt/local
-    M_dot_m="2.6"
+    install_macports $PREFIX
 
-    Mm=`echo $M_dot_m | tr -d '.'`
-    PY="py$Mm"
+    VERSION="2.6"
+    port_install_python $VERSION
+    sudo pip install matplotlib
 
-    curl https://distfiles.macports.org/MacPorts/$MACPORTS.tar.gz > $MACPORTS.tar.gz --insecure
-    tar -xzf $MACPORTS.tar.gz
+elif [ "$TEST" == "macports_py27" ]
+then
+    PREFIX=/opt/local
+    install_macports $PREFIX
 
-    cd $MACPORTS
-    ./configure --prefix=$PREFIX
-    make 
-    sudo make install
-    cd ..
+    VERSION="2.7"
+    port_install_python $VERSION
+    sudo pip install matplotlib
 
-    export PATH=$PREFIX/bin:$PATH
-    sudo port -v selfupdate
+elif [ "$TEST" == "macports_py32" ]
+then
+    PREFIX=/opt/local
+    install_macports $PREFIX
 
-    sudo port install python$Mm $PY-numpy libpng freetype
-    sudo port select --set python python$Mm
+    VERSION="3.2"
+    port_install_python $VERSION
+    sudo pip install matplotlib
 
-    # remove for mpl
-    sudo port install $PY-nose $PY-pip  # remove for mpl
-    which nosetests-$M_dot_m
-    which pip
-    sudo ln -s $PREFIX/bin/nosetests-$M_dot_m $PREFIX/bin/nosetests
-    sudo ln -s $PREFIX/bin/pip-$M_dot_m $PREFIX/bin/pip
-    pip install matplotlib
+elif [ "$TEST" == "macports_py33" ]
+then
+    PREFIX=/opt/local
+    install_macports $PREFIX
+
+    VERSION="3.3"
+    port_install_python $VERSION
+    sudo pip install matplotlib
 else
     echo "Unknown test setting ($TEST)"
 fi
