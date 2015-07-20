ed126772280a7027b1558ded03e0490cc1272a0d
Matthew Brett <matthew.brett@gmail.com>
2015-07-20 20:27:38 +0100

    Make repath_lib_names.py iterate over passed files

--- repath_lib_names.py
+++ repath_lib_names.py
@@ -2,9 +2,10 @@
 """
 
 USAGE = """\
-USAGE: repath_lib_names.py <lib_fname> <old_path> <new_path>
+USAGE: repath_lib_names.py <old_path> <new_path> <lib_fname> [<lib_fname, ...]
 """
 
+from os.path import islink, isfile
 import sys
 
 from delocate.tools import (get_install_names, set_install_name,
@@ -14,21 +15,21 @@ from delocate.tools import (get_install_names, set_install_name,
 def repath_lib(lib_fname, old_path, new_path):
     install_id = get_install_id(lib_fname)
     L = len(old_path)
-    if install_id.startswith(old_path):
+    if install_id and install_id.startswith(old_path):
         set_install_id(lib_fname, new_path + install_id[L:])
     for name in get_install_names(lib_fname):
-        print(name)
         if name.startswith(old_path):
             set_install_name(lib_fname, name, new_path + name[L:])
 
 
 def main():
-    try:
-        lib_fname, old_path, new_path = sys.argv[1:]
-    except (IndexError, ValueError):
+    if len(sys.argv) < 4:
         print(USAGE)
         sys.exit(-1)
-    repath_lib(lib_fname, old_path, new_path)
+    old_path, new_path = sys.argv[1:3]
+    for lib_fname in sys.argv[3:]:
+        if isfile(lib_fname) and not islink(lib_fname):
+            repath_lib(lib_fname, old_path, new_path)
 
 
 if __name__ == '__main__':
