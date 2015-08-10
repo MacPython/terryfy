8bb673f4410819df06920fdcfd24e18d235d84f7
Matthew Brett <matthew.brett@gmail.com>
2015-08-10 12:03:22 +0100

    BF: Fix bug in finding latest tag + test
    Also - refactor git testing file a little.

--- test_git_utils.sh
+++ test_git_utils.sh
@@ -1,22 +1,28 @@
 # Check git commit utilities
 # Check commit finding
 # Clean any old repos from previous test run
-rm -rf repo-testing
+REPO_ROOT=repo-testing
+REPO_SDIR=a_repo
+
+rm -rf $REPO_ROOT
 
 # Make new test repo
-mkdir repo-testing
-cd repo-testing
-mkdir a_repo
-cd a_repo
+mkdir $REPO_ROOT
+cd $REPO_ROOT
+mkdir $REPO_SDIR
+cd $REPO_SDIR
 git init
 git commit --allow-empty -m 'initial'
 FIRST=$(git rev-parse HEAD)
 git branch first-branch
 git checkout first-branch
+git commit --allow-empty -m 'filler-commit'
+FILLER=$(git rev-parse HEAD)
 git commit --allow-empty -m 'on-first-branch-1'
 git tag off-first -m 'off-first'
 OFF_FIRST=$(git rev-parse HEAD)
 git commit --allow-empty -m 'on-first-branch-2'
+AFTER_OFF_FIRST=$(git rev-parse HEAD)
 git checkout master
 git commit --allow-empty -m 'second'
 git tag second -m 'second tag'
@@ -42,7 +48,7 @@ git push --all origin
 cd ..
 
 function check_hash {
-    cd a_repo
+    cd $REPO_SDIR
     actual_commit=`git rev-parse HEAD`
     if [[ $actual_commit != $1 ]]; then
         echo "$actual_commit != $1"
@@ -52,30 +58,40 @@ function check_hash {
 }
 
 # Checkout commit hash
-checkout_commit a_repo $SECOND
+checkout_commit $REPO_SDIR $SECOND
 check_hash $SECOND
 # Checkout default (master)
-checkout_commit a_repo
+checkout_commit $REPO_SDIR
 check_hash $MASTER
 # Checkout non-default branch
-checkout_commit a_repo other
+checkout_commit $REPO_SDIR other
 check_hash $OTHER
 # Checkout a tag name
-checkout_commit a_repo second
+checkout_commit $REPO_SDIR second
 check_hash $SECOND
 # Checkout latest tag on default branch (master)
-checkout_commit a_repo $OTHER  # go somewhere else first
-checkout_commit a_repo latest-tag
+checkout_commit $REPO_SDIR $OTHER  # go somewhere else first
+checkout_commit $REPO_SDIR latest-tag
 check_hash $SECOND
 # Check other branch, and tag direct extension from branch
-checkout_commit a_repo latest-tag other
+checkout_commit $REPO_SDIR latest-tag other
 check_hash $EARLY_TAG
 # Check earlier commit hash for tag source
-checkout_commit a_repo latest-tag $SECOND
+checkout_commit $REPO_SDIR latest-tag $SECOND
 check_hash $SECOND
-checkout_commit a_repo latest-tag first-branch
+checkout_commit $REPO_SDIR latest-tag first-branch
 check_hash $OFF_FIRST
+checkout_commit $REPO_SDIR latest-tag $FILLER
+check_hash $OFF_FIRST
+# Add an extra tag to early branch
+cd $REPO_SDIR
+git tag after-off-first -am "Message" $AFTER_OFF_FIRST
+cd ..
+# Check we get the second of the two tags now
+checkout_commit $REPO_SDIR
+checkout_commit $REPO_SDIR latest-tag $FILLER
+check_hash $AFTER_OFF_FIRST
 
 # Clean up after
 cd ..
-rm -rf repo-testing
+rm -rf $REPO_ROOT
--- test_tools.sh
+++ test_tools.sh
@@ -1,5 +1,6 @@
 # Final return code variable used by sub-scripts
 RET=0
+# ``source travis_utils.sh`` required before these commands
 source test_python_installs.sh
 source test_pyver_ge.sh
 source test_git_utils.sh
