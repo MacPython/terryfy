b6414846474ed3b90fb0aa8299270b7fd93036a1
Matthew Brett <matthew.brett@gmail.com>
2020-01-11 13:59:41 +0000

    Remove old Python versions that can't be filled.

--- test_fill_pyver.sh
+++ test_fill_pyver.sh
@@ -2,13 +2,8 @@
 if [ $(fill_pyver 2) != $LATEST_2p7 ]; then RET=1; fi
 if [ $(fill_pyver 2.7) != $LATEST_2p7 ]; then RET=1; fi
 if [ $(fill_pyver 2.7.8) != "2.7.8" ]; then RET=1; fi
-if [ $(fill_pyver 2.6) != $LATEST_2p6 ]; then RET=1; fi
-if [ $(fill_pyver 2.6.2) != "2.6.2" ]; then RET=1; fi
 if [ $(fill_pyver 3) != $LATEST_3p6 ]; then RET=1; fi
 if [ $(fill_pyver 3.6) != $LATEST_3p6 ]; then RET=1; fi
 if [ $(fill_pyver 3.6.0) != "3.6.0" ]; then RET=1; fi
 if [ $(fill_pyver 3.5) != $LATEST_3p5 ]; then RET=1; fi
 if [ $(fill_pyver 3.5.0) != "3.5.0" ]; then RET=1; fi
-if [ $(fill_pyver 3.4) != $LATEST_3p4 ]; then RET=1; fi
-if [ $(fill_pyver 3.3) != $LATEST_3p3 ]; then RET=1; fi
-if [ $(fill_pyver 3.2) != $LATEST_3p2 ]; then RET=1; fi
